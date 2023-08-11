279b14f26e8456b6e173b04962f7ea1fae554c3e
synpixel <syntaxwastaken2@gmail.com>
2023-08-11 03:06:13 +0200

    add typings

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+export interface UserInfo {
+	description: string;
+	created: string;
+	isBanned: boolean;
+	externalAppDisplayName?: string;
+	hasVerifiedBadge: boolean;
+	id: number;
+	name: string;
+	displayName: string;
+}
+
+export interface FakePlayer {
+	UserId: number;
+	Name: string;
+	DisplayName: string;
+}
+
+export declare const getUserInfo: () => UserInfo;
+export declare const getUserId: () => number;
+export declare const getUsername: () => string;
+export declare const getDisplayName: () => string;
+export declare const getUserThumbnail: (thumbnailType: Enum.ThumbnailType, thumbnailSize: Enum.ThumbnailSize) => string;
+export declare const createPlayer: () => FakePlayer;
+export declare const createList: <TReturn>(fn: () => TReturn, count: number) => TReturn[];
